452d4996043fcda655ff190e9839f50a9ac2fada
Michael Nwankwo <mikhaelnwankwo@gmail.com>
2021-07-11 09:17:04 +0100

    bug fixes

--- src/services/http.js
+++ src/services/http.js
@@ -1,8 +1,6 @@
 import axios from 'axios'
 
-// let serverUrl = 'https://survey-ninja-server.herokuapp.com'
-
-let serverUrl = 'http://localhost:3002'
+let serverUrl = 'https://survey-ninja-server.herokuapp.com'
 
 const http = () => {
     return axios.create({
